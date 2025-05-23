7545059e7a2ec1ecd49343a10731f2e600b3a545
Ahmed Mohamed <ahmedmohuammed.dev@gmail.com>
2025-05-24 01:10:16 +0300

    Clean up Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,43 +1,21 @@
 import watches from "../data/watches.json";
-import RatingStars from "./RatingStars";
+import WatchCard from "./WatchCard";
 
 function Main() {
   return (
     <main className="flex-1 w-full mb-4">
       <h3 className="text-md text-gray-600 mb-4">{watches.length} Products</h3>
-      <div className="products">
-        {watches.map((watch, i) => {
-          return (
-            <div
-              className="border-2 border-gray-400 rounded py-2 px-4 cursor-pointer transition-all duration-200 hover:shadow-md"
-              key={watch.id}
-            >
-              <div className="image w-full relative overflow-hidden h-60">
-                <img
-                  src={watch.image}
-                  alt={`Watch number ${i + 1}`}
-                  className="w-full h-full object-cover"
-                  loading="lazy"
-                />
-              </div>
-              <div className="py-4">
-                <p className="title text-lg">{watch.name}</p>
-                <p className="brand text-gray-600 text-sm mb-4">
-                  {watch.brand}
-                </p>
-                <div className="rating-and-price">
-                  <div className="rating flex items-center justify-between">
-                    <p className="price font-bold">${watch.price}</p>
-                    <div className="stars">
-                      <RatingStars rating={watch.rating} />
-                    </div>
-                    <p className="rating-number">{watch.rating}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+      <div className="watches-grid">
+        {watches.map(({ id, name, brand, image, price, rating }) => (
+          <WatchCard
+            key={id}
+            name={name}
+            brand={brand}
+            image={image}
+            price={price}
+            rating={rating}
+          />
+        ))}
       </div>
     </main>
   );
---
+++ src/components/WatchCard.tsx
@@ -0,0 +1,41 @@
+import RatingStars from "./RatingStars";
+
+export interface WatchCardProps {
+  image: string;
+  name: string;
+  brand: string;
+  price: number;
+  rating: number;
+}
+
+export default function WatchCard({
+  image,
+  name,
+  brand,
+  price,
+  rating,
+}: WatchCardProps) {
+  return (
+    <div className="border-2 border-gray-400 rounded py-2 px-4 cursor-pointer transition-all duration-200 hover:shadow-md">
+      <div className="image w-full relative overflow-hidden h-60">
+        <img
+          src={image}
+          alt={`Watch Image ${name}`}
+          className="w-full h-full object-cover"
+          loading="lazy"
+        />
+      </div>
+      <div className="py-4">
+        <p className="title text-lg">{name}</p>
+        <p className="brand text-gray-600 text-sm mb-4">{brand}</p>
+        <div className="details rating flex items-center">
+          <p className="price font-bold">${price}</p>
+          <div className="stars flex-1 flex justify-end mr-1">
+            <RatingStars rating={rating} />
+          </div>
+          <p className="rating-number">{rating}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
