fdee9310b4f453d353817b17f10f2b9bc7762b98
Ahmed Mohamed <ahmedmohuammed.dev@gmail.com>
2025-05-25 02:37:03 +0300

    trying to fix deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/e-commerce",
+  base: "/e-commerce/",
   plugins: [react(), tailwindcss()],
 });
