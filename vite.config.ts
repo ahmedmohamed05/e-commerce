7771a3cc8649b75a227e8b3f77aeb04d2a8e201c
Ahmed Mohamed <ahmedmohuammed.dev@gmail.com>
2025-05-26 15:46:06 +0300

    edit base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/e-commerce/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
